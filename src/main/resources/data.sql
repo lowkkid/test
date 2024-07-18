05c9c88656feb8acefa3a8113d94b9800e8b0da2
lowkkid <lowkkidvstheworld@gmail.com>
2024-07-18 13:54:23 +0300

    .

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,10 +9,10 @@ INSERT INTO author (first_name, last_name, birth_date) VALUES ('Jane', 'Austen',
 INSERT INTO author (first_name, last_name, birth_date) VALUES ('Mark', 'Twain', '1835-11-30');
 INSERT INTO author (first_name, last_name, birth_date) VALUES ('Fyodor', 'Dostoevsky', '1821-11-11');
 
-INSERT INTO book (name, release_date, author_id) VALUES ('1984', '1949-06-08', 100000); -- George Orwell
-INSERT INTO book (name, release_date, author_id) VALUES ('Animal Farm', '1945-08-17', 100000); -- George Orwell
-INSERT INTO book (name, release_date, author_id) VALUES ('Brave New World', '1932-08-30', 100001); -- Aldous Huxley
-INSERT INTO book (name, release_date, author_id) VALUES ('Pride and Prejudice', '1813-01-28', 100002); -- Jane Austen
-INSERT INTO book (name, release_date, author_id) VALUES ('Adventures of Huckleberry Finn', '1884-12-10', 100003); -- Mark Twain
-INSERT INTO book (name, release_date, author_id) VALUES ('The Brothers Karamazov', '1880-11-01', 100004); -- Fyodor Dostoevsky
-INSERT INTO book (name, release_date, author_id) VALUES ('Crime and Punishment', '1866-01-01', 100004); -- Fyodor Dostoevsky
+INSERT INTO book (name, release_date, author_id) VALUES ('1984', '1949-06-08', 100000);
+INSERT INTO book (name, release_date, author_id) VALUES ('Animal Farm', '1945-08-17', 100000);
+INSERT INTO book (name, release_date, author_id) VALUES ('Brave New World', '1932-08-30', 100001);
+INSERT INTO book (name, release_date, author_id) VALUES ('Pride and Prejudice', '1813-01-28', 100002);
+INSERT INTO book (name, release_date, author_id) VALUES ('Adventures of Huckleberry Finn', '1884-12-10', 100003);
+INSERT INTO book (name, release_date, author_id) VALUES ('The Brothers Karamazov', '1880-11-01', 100004);
+INSERT INTO book (name, release_date, author_id) VALUES ('Crime and Punishment', '1866-01-01', 100004);
